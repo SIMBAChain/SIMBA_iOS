06d77b73d67847d48ba91d8a308e3558b135d8cb
Steven Peregrine <stevefperegrine@gmail.com>
2018-04-09 13:01:07 -0400

    test2

--- HealthSpaceSIMBA_iOS/ViewController.swift
+++ HealthSpaceSIMBA_iOS/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Test")
+        print("Test2")
     }
 
     override func didReceiveMemoryWarning() {
