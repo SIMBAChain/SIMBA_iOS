a3502bec9111abed6198bf7738b730b1b1ef20a4
Lancewise <Lancewise@users.noreply.github.com>
2018-04-09 13:12:00 -0400

    Tests Completed. Project set back to initial setup

--- HealthSpaceSIMBA_iOS/ViewController.swift
+++ HealthSpaceSIMBA_iOS/ViewController.swift
@@ -8,15 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Test2")
+        
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
