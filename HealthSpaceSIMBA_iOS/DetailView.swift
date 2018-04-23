aa6029db53ab1a05b5d4518f8ea8e8b87090f187
Steven Peregrine <stevefperegrine@gmail.com>
2018-04-23 13:50:50 -0400

    -Added outlets to detail view -Misc.

--- HealthSpaceSIMBA_iOS/DetailView.swift
+++ HealthSpaceSIMBA_iOS/DetailView.swift
@@ -10,5 +10,17 @@ import Foundation
 import UIKit
 class DetailViewController: UIViewController
 {
+    @IBOutlet  var IPFS: UITextField!
+    @IBOutlet  var TimeStamp: UITextField!
+    @IBOutlet  var Location: UITextField!
+    @IBOutlet  var Name: UITextField!
+    @IBOutlet  var Desc: UITextView!
+    @IBOutlet  var Status: UITextField!
+    @IBOutlet  var Comments: UITextView!
+    @IBOutlet  var VerStatus: UITextField!
+    @IBOutlet  var FirstAudit: UITextField!
+    @IBOutlet  var SecondAudit: UITextField!
+    @IBOutlet  var CorrectButton: UIButton!
+    @IBOutlet  var IncorrectButton: UIButton!
     
 }
